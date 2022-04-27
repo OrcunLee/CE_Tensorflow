62c0894498d3ed2bcc871bfa97f6a96e90bb2e5e
Hot6x <95900411+Hot6x@users.noreply.github.com>
2022-04-27 19:46:17 +0900

    .

--- 3_Boston.py
+++ 3_Boston.py
@@ -1,35 +1,33 @@
-#%% 라이브러리
 import tensorflow as tf
 import pandas as pd
 
-#%% 
-보스턴 = pd.read_csv("C:/Users/yzz07/Desktop/PROGRAMMING/Training_Tensorflow/보스턴.csv")
-print(보스턴.shape)
-print(보스턴.columns)
-print(보스턴.head())
+Boston = "https://raw.githubusercontent.com/Hot6x/Tensorflow_CE/main/Data/Boston.csv"
+Boston = pd.read_csv(Boston)
 
-# %%
-독립 = 보스턴[['crim', 'zn', 'indus', 'chas', 'nox', 
+print(Boston.shape)
+print(Boston.columns)
+print(Boston.head())
+
+독립 = Boston[['crim', 'zn', 'indus', 'chas', 'nox', 
             'rm', 'age', 'dis', 'rad', 'tax',
             'ptratio', 'b', 'lstat']]
-종속 = 보스턴[['medv']]
+종속 = Boston[['medv']]
 print(독립.shape, 종속.shape)
 
-# %% 모델 생성
+
 X = tf.keras.layers.Input(shape=[13])
 H = tf.keras.layers.Dense(10, activation='swish')(X)
 Y = tf.keras.layers.Dense(1)(H)
 model = tf.keras.models.Model(X, Y)
 model.compile(loss='mse')
 
-# %% 모델을 학습
+
 model.fit(독립, 종속, epochs=1000, verbose=0)   # verbose = 0 은 화면출력 안한다는 명령
 model.fit(독립, 종속, epochs=10)    # epochs 학습활 횟수
-#%%
 model.summary()
-# %%
+
 print(model.predict(독립[5:10]))  # 독립 변수일때 종속변수 값 예측
 print(종속[5:10]) # 종속변수 확인
 
-# %% 모델의 수식 확인
-print(model.get_weights())
+
+print(model.get_weights()) #모델의 수식 확인
--- 4_Irese.py
+++ 4_Irese.py
@@ -1,21 +1,20 @@
-#%% 라이브러리
 import tensorflow as tf
 import pandas as pd
 
-아이리스 = pd.read_csv("C:/Users/yzz07/Desktop/PROGRAMMING/Training_Tensorflow/아이리스.csv")
-print(아이리스.shape)
-print(아이리스.columns)
-print(아이리스.head(10))
-print(아이리스.tail(10))
+Irese= "https://raw.githubusercontent.com/Hot6x/Tensorflow_CE/main/Data/Irese.csv"
+Irese = pd.read_csv(Irese)
+print(Irese.shape)
+print(Irese.columns)
+print(Irese.head(10))
+print(Irese.tail(10))
 
-인코딩 = pd.get_dummies(아이리스)
+인코딩 = pd.get_dummies(Irese)
 인코딩.head()
 
-독립 = 인코딩[['꽃잎길이', '꽃잎폭', '꽃받침길이', '꽃받침폭']]
-종속 = 인코딩[['품종_setosa', '품종_versicolor', '품종_virginica']]
+독립 = 인코딩[['petal_length', 'petal_width', 'calyx_length', 'calyx_width']]
+종속 = 인코딩[['kind_setosa', 'kind_versicolor', 'kind_virginica']]
 print(독립.shape, 종속.shape)
 
-# %% 
 X = tf.keras.layers.Input(shape=[4])
 H = tf.keras.layers.Dense(8, activation="swish")(X)
 H = tf.keras.layers.Dense(8, activation="swish")(H)
@@ -24,22 +23,15 @@ Y = tf.keras.layers.Dense(3, activation='softmax')(H)
 model = tf.keras.models.Model(X, Y)
 model.compile(loss='categorical_crossentropy', metrics='accuracy')
 
-# %% 학습
 model.fit(독립, 종속, epochs=1000, verbose=0)
 model.fit(독립, 종속, epochs=10)
 
-# %% 확인
 
-# 맨 처음 데이터 5개
 print(model.predict(독립[:5]))
 print(종속[:5])
- 
-# 맨 마지막 데이터 5개
+
 print(model.predict(독립[-5:]))
 print(종속[-5:])
 
-# %%
 # weights & bias 출력
 print(model.get_weights())
-
-# %%
